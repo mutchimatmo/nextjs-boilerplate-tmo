a64ea50715c9eedf0b5471f90ee191ba71412f0f
Mutchima Tangmo <mutchimatmo@gmail.com>
2023-05-21 12:58:37 +0000

    Update 5 files

---
+++ components/PosterPart.js
@@ -0,0 +1,45 @@
+function PosterPart() {
+  return (
+    <div id="posterpart">
+      <div id="title">
+        <div class="group-name">Borsud Christmas</div>
+        <div class="share">
+          <div class="code">tmo26x</div>
+          <div class="icon-wrap">
+            <img src="/img/link.svg" />
+          </div>
+        </div>
+      </div>
+      
+      <div id="group">
+        <div class="group-information">
+          <div class="information">
+            <div class="topic">Date rage</div>
+            <div class="detail">10 - 30 May 2050</div>
+          </div>
+          <div class="information">
+            <div class="topic">Budget for a gift</div>
+            <div class="detail">800 - 1,000 baht</div>
+          </div>
+          <div class="information">
+            <div class="topic">Detail</div>
+            <div class="detail">Sea theme</div>
+          </div>
+        </div>
+        
+        <img src="/img/name.png" width="90%" />
+        
+        <div class="save">
+          <div class="save-description">
+            The poster shows members initials and their chosen colors.
+          </div>
+          <div class="save-button">
+            <div class="save-poster">Save poster</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PosterPart;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Nav from "../components/nav";
 import Head from "next/head";
+import PosterPart from "@/components/PosterPart";
 
 function Home() {
   return (
     <div>
       <Head>
         <title>Tech Dsgn Exp</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Nav />
-      <h1 className="text-9xl">Tangmo</h1>
+      <PosterPart />
     </div>
   );
 }
